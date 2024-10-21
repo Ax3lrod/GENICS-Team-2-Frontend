86a9a5696c55ed9183707f11c61a82875c05d594
nawa316 <muhammadadedzakwan@gmail.com>
2024-10-21 16:00:38 +0700

    fix: provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className='antialiased'
       >
         <Providers>{children}</Providers>
       </body>
